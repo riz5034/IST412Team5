4fd98b1b32ef409e11a55efed50c70943abab45b
jpenning7 <jep5455@psu.edu>
2018-11-08 18:26:52 -0500

    Added project description text to about page. Descriptions were taken from the statement of work document.

---
+++ WebLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
